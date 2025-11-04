58cbf2b2232aa17cca029faa62ff7311aa0d4de6
mahmoud.elsharkawy <mahmoud.elsharkawy@gizasystems.com>
2025-11-04 14:23:22 +0200

    Add TC2 Valid Login

---
+++ tests/LoginTests.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect, Page, BrowserContext } from '@playwright/test';
+import * as allure from 'allure-js-commons';
+import { LoginPage } from '../pages/LoginPage'
+import { HeaderPage } from '../pages/HeaderPage';
+import { HomePage } from '../pages/HomePage';
+import { ApisUserManagement } from '../apis/ApisUserManagement';
+import * as fs from 'fs';
+
+let context: BrowserContext;
+let page: Page;
+
+let loginPage: LoginPage;
+let homePage: HomePage;
+let headerPage: HeaderPage;
+let apisUserManagement: ApisUserManagement;
+
+let testData: any;
+
+const timestamp = new Date().toISOString().replace(/[-T:.]/g, "").slice(0, 17);
+
+test.describe('Automation Exercise Login Test Cases', () => {
+
+  test('Test Case 2: Login User with correct email and password', async () => {
+    allure.feature('Automation Exercise Login Test Cases');
+    allure.tms('1111');
+    // allure.issue('#link');
+
+    const email = testData.emailAddress + timestamp + '@test.com';
+    await homePage.navigate();
+    await headerPage.clickOnSignupLoginLink();
+    await loginPage.login(email, testData.password);
+    await headerPage.assertUserLoggedinSuccessfully(testData.username);
+  });
+
+  test.beforeAll(async () => {
+    testData = JSON.parse(fs.readFileSync('./resources/test-data/LoginTestJsonFile.json', 'utf8'));
+  });
+
+  test.beforeEach(async ({ request, browser }) => {
+    apisUserManagement = new ApisUserManagement(request);
+    await apisUserManagement.createUser(testData.username, testData.emailAddress + timestamp + '@test.com', testData.password)
+
+    context = await browser.newContext();
+    page = await context.newPage();
+    loginPage = new LoginPage(page);
+    homePage = new HomePage(page);
+    headerPage = new HeaderPage(page);
+  });
+
+  test.afterEach(async () => {
+    await context.close();
+  });
+
+});
