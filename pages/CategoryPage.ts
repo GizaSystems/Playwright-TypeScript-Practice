8f8e56e9f84cee35c1f472f009e8f90a75eaa912
Rana Adel <ranaadel1964@gmail.com>
2025-11-24 15:32:05 +0200

    enhaced a locator

--- pages/CategoryPage.ts
+++ pages/CategoryPage.ts
@@ -8,7 +8,7 @@ export class CategoryPage {
   constructor(page: Page) {
     this.page = page;
     //  Locators
-    this.categoryHeader = page.locator('h2.category-header');
+    this.categoryHeader = page.locator('div.sidebar h2.category-header');
     this.categoryPage_heading = page.locator('h2.title.text-center');
   }
   //locator helper
